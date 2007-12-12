a3b4556634265abf1a646de3d9c74f43bb76c3af
James Coglan <jcoglan@googlemail.com>
2007-12-12 02:31:52 +0000

    Added support for Methodphitamine-inspired it/its constructs. Includes 240 predefined method names pulled from the Mozilla core JavaScript reference.

--- reiterate.src.js
+++ reiterate.src.js
@@ -88,6 +88,118 @@ Hash.prototype.toFunction = function() {
   };
 };
 
+Function.ChainCollector = function() {
+  var CLASS = arguments.callee;
+  
+  this.then = this.and = this;
+  var queue = [], baseObject = arguments[0];
+  
+  this.____ = function(method, args) {
+    queue.push({func: method, args: args});
+  };
+  
+  this.fire = function(base) {
+    var object = base || baseObject, method, property;
+    for (var i = 0, n = queue.length; i < n; i++) {
+      method = queue[i];
+      if (object instanceof CLASS) {
+        object.____(method.func, method.args);
+        continue;
+      }
+      property = object[method.func];
+      object = (typeof property == 'function')
+          ? property.apply(object, method.args)
+          : property;
+    }
+    return object;
+  };
+  
+  this.toFunction = function() {
+    var chain = this;
+    return function(o) { return chain.fire(o); };
+  };
+  
+  for (var i = 0, n = arguments.length; i < n; i++)
+    CLASS.addMethods(this, arguments[i]);
+};
+
+Function.ChainCollector.addMethods = function(self, object) {
+  var methods = [], property, i, n, name;
+  
+  var reservedNames = [], blank = new this();
+  for (property in blank) reservedNames.push(property);
+  var re = new RegExp('^' + reservedNames.join('|') + '$');
+  
+  for (property in object) {
+    if (Number(property) != property)
+      methods.push(property);
+  }
+  if (object instanceof Array) {
+    for (i = 0, n = object.length; i < n; i++) {
+      if (typeof object[i] == 'string')
+        methods.push(object[i]);
+    }
+  }
+  for (i = 0, n = methods.length ; i < n; i++)
+    (function(name) {
+      if (re.test(name)) return;
+      self[name] = function() {
+        this.____(name, arguments);
+        return this;
+      };
+    })(methods[i]);
+  
+  if (object.prototype)
+    this.addMethods(self, object.prototype);
+};
+
+Function.ALL_METHODS = [
+  "__defineGetter__", "__defineSetter__", "__lookupGetter__", "__lookupSetter__", "abbr", 
+  "abs", "accept", "acceptCharset", "accesskey", "acos", "action", "align", "alink", "alt", 
+  "apply", "archive", "arity", "asin", "atan", "atan2", "axis", "background", "bgcolor", 
+  "border", "call", "caller", "ceil", "cellpadding", "cellspacing", "char", "charAt", 
+  "charCodeAt", "charoff", "charset", "checked", "cite", "className", "classid", "clear", 
+  "code", "codebase", "codetype", "color", "cols", "colspan", "compact", "concat", "content", 
+  "coords", "cos", "data", "datetime", "declare", "defer", "dir", "disabled", "enctype", 
+  "every", "exec", "exp", "face", "filter", "floor", "forEach", "frame", "frameborder", 
+  "fromCharCode", "getDate", "getDay", "getFullYear", "getHours", "getMilliseconds", 
+  "getMinutes", "getMonth", "getSeconds", "getTime", "getTimezoneOffset", "getUTCDate", 
+  "getUTCDay", "getUTCFullYear", "getUTCHours", "getUTCMilliseconds", "getUTCMinutes", 
+  "getUTCMonth", "getUTCSeconds", "getYear", "global", "hasOwnProperty", "headers", "height", 
+  "href", "hreflang", "hspace", "htmlFor", "httpEquiv", "id", "ignoreCase", "index", 
+  "indexOf", "innerHTML", "input", "isPrototypeOf", "ismap", "join", "label", "lang", "language", 
+  "lastIndex", "lastIndexOf", "length", "link", "log", "longdesc", "map", "marginheight", 
+  "marginwidth", "match", "max", "maxlength", "media", "method", "min", "multiline", 
+  "multiple", "name", "nohref", "noresize", "noshade", "now", "nowrap", "object", "onblur", 
+  "onchange", "onclick", "ondblclick", "onfocus", "onkeydown", "onkeypress", "onkeyup", 
+  "onload", "onmousedown", "onmousemove", "onmouseout", "onmouseover", "onmouseup", 
+  "onreset", "onselect", "onsubmit", "onunload", "parse", "pop", "pow", "profile", "prompt", 
+  "propertyIsEnumerable", "push", "random", "readonly", "reduce", "reduceRight", "rel", 
+  "replace", "rev", "reverse", "round", "rows", "rowspan", "rules", "scheme", "scope", 
+  "scrolling", "search", "selected", "setDate", "setFullYear", "setHours", "setMilliseconds", 
+  "setMinutes", "setMonth", "setSeconds", "setTime", "setUTCDate", "setUTCFullYear", 
+  "setUTCHours", "setUTCMilliseconds", "setUTCMinutes", "setUTCMonth", "setUTCSeconds", 
+  "setYear", "shape", "shift", "sin", "size", "slice", "some", "sort", "source", "span", 
+  "splice", "split", "sqrt", "src", "standby", "start", "style", "substr", "substring", 
+  "summary", "tabindex", "tan", "target", "test", "text", "title", "toExponential", 
+  "toFixed", "toGMTString", "toLocaleDateString", "toLocaleFormat", "toLocaleString", 
+  "toLocaleTimeString", "toLowerCase", "toPrecision", "toSource", "toString", "toUTCString", 
+  "toUpperCase", "type", "unshift", "unwatch", "usemap", "valign", "value", "valueOf", 
+  "valuetype", "version", "vlink", "vspace", "watch", "width"
+];
+
+var it = its = function() {
+  var chain = new Function.ChainCollector(Function.ALL_METHODS, Array, Date,
+      Element.Methods, Element.Methods.Simulated, Enumerable, Event, Form,
+      Form.Element, Function, Hash, Number, Object, ObjectRange, Position,
+      String, Template);
+      
+  for (var i = 0, n = arguments.length; i < n; i++)
+    Function.ChainCollector.addMethods(chain, arguments[i]);
+  
+  return chain;
+};
+
 [Enumerable, Array.prototype, Hash.prototype, ObjectRange.prototype,
 Ajax.Responders, Element.ClassNames.prototype].each(function(object) {
   $w('all any collect detect findAll max min partition reject sortBy map find select filter every some').each(function(method) {
